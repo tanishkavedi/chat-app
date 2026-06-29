4dab5e3127ffb0d134266597347e93ac3b5557d9
tanishkavedi <160759271+tanishkavedi@users.noreply.github.com>
2026-06-29 23:56:00 +0530

    feat: configure Tailwind CSS and initialize React app

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App () {
+  return (
+    <div className="min-h-screen bg-gray-950 text-white flex items-center justify-cente">
+      <h1 className="text-3xl font-bold text-blue-400">Chat App</h1>
+    </div>
+  );
+}
+
+export default App;
